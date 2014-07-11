2a6d1a16bbb7b0fb03ec34bafb7620c21ef8d41b
bcambl <blayne@blaynecampbell.com>
2014-07-11 13:53:44 -0600

    Passwords are now 'temporary' & persistant passwords now optional
     - Passwords are now expired by default to force initial password reset.
     - Persistant passwords no longer the default. (optional addusr param)
     - Minor documentation changes

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
 fabric-adduser
 ==============
 
-Add multiple users to a list of servers while maintaining unique passwords.
+#### Fabric script for adding multiple users to a list of servers. 
+This script also allows the administrator to set the same unique password for 
+each user accross all servers if desired. (see addusr params)
+
+#####Note:
+All new accounts are expired by default and will force each user to reset
+their password upon first login. Users have approximatly 30 days to login
+and change their temporary password or the account will turn inactive.
+
+
+#### Dependancies:
+
+- fabric api
+- makepasswd utility
+
+#### Usage:
+
+Add users:
+
+```
+$ fab addusr
+```
+
+Add users with persistant passwords:
+
+```
+$ fab addusr:pp=True
+```
+
+Remove users and leave groups intact.
+
+```
+$ fab delusr
+```
+
+###### Warning: 
+Do not run as parallel as passwords are only generated once for initial server
+to allow persistant temporary password for all hosts being processed.
+
--- fabfile.py
+++ fabfile.py
@@ -1,19 +1,23 @@
-# !/usr/bin/python
+# !/usr/bin/env python
 __author__ = 'Blayne Campbell'
 __date__ = '3/25/14'
 
 ##################################################
-# Fabric script for adding multiple users to
-# a list of servers while maintaining unique
-# passwords for individual users.
+# Fabric script for adding multiple users to a list of hosts. This script
+# also allows the administrator to set the same unique password for each user
+# across all hosts if desired. (see addusr params for persistant passwords)
+#
+# All new accounts are expired by default and will force each user to reset
+# their password upon first login. Users have approximatly 30 days to login
+# and change their temporary password or the account will turn inactive.
 #
 # Usage:
-#   $ fab <addusr|delusr>
+#   $ fab -l will list commands
+#   $ fab -d <command> will provide command info & params
 #
 # Warning:
-#  Do not run as parallel task because passwords
-#  are only generated once on first pass to
-#  remain persistant for the remaining servers.
+# Do not run as parallel as passwords are only generated once for initial
+# host to allow persistant temporary password for all hosts being processed.
 #
 # Dependancies:
 #     - fabric api
@@ -28,7 +32,7 @@ import re
 from fabric.api import *
 
 
-# Target Servers
+# Target hosts
 env.hosts = ['server1', 'server2']
 
 # User List ['Full Name', 'UserID']
@@ -43,14 +47,13 @@ group = 'groupname'
 
 
 def generatepw():
-    """ Generates a random password that meets
-    Password Criteria:
+    """ Generates a random password that meets the following Password criteria:
         13 characters length
-        4 letters in Upper Case
-        3 Special Character (!@#$&*)
+        4 letters in uppercase
+        3 special character (!@#$&*)
         3 numerals (0-9)
-        3 letters in Lower Case
-    :return: Random Password that meets criteria
+        3 letters in lowercase
+    :return: Random password that meets criteria
     """
     # Only allow visual friendly characters to minimize user frustration
     allowchar = 'abcdefghijkmnpqrstuvwxyz' \
@@ -68,9 +71,16 @@ def generatepw():
 
 
 @task
-def addusr():
+def addusr(pp=None):
     """ Adds Users specified by 'users' & creates group
+    :param pp: Set True if password should be the same on all hosts
     :return: Nothing
+
+    Usage:
+        Different password for each hosts:
+        $ fab addusr
+        Persistant passwords across all hosts
+        $ fab addusr:pp=True
     """
     with settings(hide('warnings', 'stdout', 'stderr'), warn_only=True):
         addgroup = sudo("groupadd %s" % group)
@@ -91,11 +101,15 @@ def addusr():
             sudo('usermod --password %s %s' % (crypted_password,
                                                i[1]), pty=True)
             # Modify the following rules to your needs: (Read chage manpage)
-            sudo("chage -E -1 -W 11 -m 7 -M 42 -I 30 %s" % i[1])
+            sudo("chage -E -1 -W 11 -m 7 -M 42 -I 30 -d now-44days %s" % i[1])
     print("\nUsers for %s\n" % env.host)
     for i in users:  # Print the list of Users with credentials
         print("%s" % i[0])
         print("User: %s\nPassword: %s" % (i[1], i[2]))
+    if not pp:
+        for i in users:
+            if len(i) == 3:
+                i.pop()
 
 
 @task
